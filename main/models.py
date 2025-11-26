3aabbdfa2ad567d4a7c37d5bea6069794fbe80f0
Noah Patterson <noah@noahpatterson.me>
2025-11-25 19:51:17 -0700

    add models, views, urls, forms

--- main/models.py
+++ main/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    due_date = models.DateTimeField(blank=True, null=True)
+    category = models.ForeignKey("Category", on_delete=models.CASCADE, blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title 
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    color = models.CharField(max_length=7, default="#FFFFFF")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
